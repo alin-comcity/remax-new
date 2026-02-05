ba6d66411fd6d4c19d65c5a439470d74d7be870b
Masum <masum@Masums-Mac-mini.local>
2026-02-05 10:39:29 +0600

    cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false,
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+});
